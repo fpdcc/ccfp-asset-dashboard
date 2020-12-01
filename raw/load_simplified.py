65cd75162731a7538054be929cfa726b6e7d0d85
Sam McAlilly <sammcalilly@gmail.com>
2020-12-01 10:00:00 -0600

    debugging module imports

--- raw/load_simplified.py
+++ raw/load_simplified.py
@@ -1,19 +1,20 @@
 import json
-from asset_dashboard.models import Project
+import csv
+from ..asset_dashboard.models import Project
 
-# i executed the code in the docker container
-# so the paths/modules here might not work
-with open('simplified.json') as json_file:
-    data = json.load(json_file)
-    
-    for project in data['projects']:
-        p = Project(
-            name=project['name'],
-            project_description=project['project_description'],
-            budget=project['budget'],
-            zone=project['zone']
-        )
+with open('raw/simplified.csv', newline='') as csv_file:
+    reader = csv.DictReader(csv_file)
+    for row in reader:
+        print(row['name'])
+        print(row['project_description'])
+        print(row['budget'])
+        print(row['zone'])
+        # p = Project.objects.create(
+        #     name=row['name'],
+        #     project_description=row['project_description'],
+        #     budget=row['budget'],
+        #     zone=row['zone']
+        # )
 
-        p.save()
         
     print('data saved into the database. now dump it into a fixture.')
