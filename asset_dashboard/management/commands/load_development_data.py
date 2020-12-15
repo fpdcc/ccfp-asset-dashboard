c9ca3891fd4fce4c6cb54886542efd40cd8e681f
Sam McAlilly <sammcalilly@gmail.com>
2020-12-15 11:18:54 -0600

    fixing data upload bug

--- asset_dashboard/management/commands/load_development_data.py
+++ asset_dashboard/management/commands/load_development_data.py
@@ -7,19 +7,21 @@ class Command(BaseCommand):
     help = 'Loads in dummy data for local development'
 
     def handle(self, *args, **options):
-        with open('raw/simplified.csv', newline='') as csv_file:
-            reader = csv.DictReader(csv_file)
-            
-            count = 0
+        dummy_projects = DummyProject.objects.all()
+        if len(dummy_projects) > 0:
+            with open('raw/simplified.csv', newline='') as csv_file:
+                reader = csv.DictReader(csv_file)
+                
+                count = 0
 
-            for row in reader:
-                p = DummyProject.objects.create(
-                    name=row['name'],
-                    project_description=row['project_description'],
-                    budget=row['budget'],
-                    zone=row['zone']
-                )
-                count += 1
+                for row in reader:
+                    p = DummyProject.objects.create(
+                        name=row['name'],
+                        project_description=row['project_description'],
+                        budget=row['budget'],
+                        zone=row['zone']
+                    )
+                    count += 1
 
-            print(f'{count} objects saved to your local database. Happy development!')
+                print(f'{count} objects saved to your local database. Happy development!')
             
