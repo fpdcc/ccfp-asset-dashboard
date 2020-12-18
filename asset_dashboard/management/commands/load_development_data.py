d4f6f32aa8193de1890e0d5de85334ef4d998fd8
Sam McAlilly <sammcalilly@gmail.com>
2020-12-18 13:38:54 -0600

    improve dummy data load command

--- asset_dashboard/management/commands/load_development_data.py
+++ asset_dashboard/management/commands/load_development_data.py
@@ -8,21 +8,17 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
-        dummy_projects = DummyProject.objects.all()
+        dummy_projects = DummyProject.objects.all().delete()
 
-        if len(dummy_projects) == 0:
-            with open('raw/simplified.csv', newline='') as csv_file:
-                reader = csv.DictReader(csv_file)
-                
-                count = 0
+        with open('raw/simplified.csv', newline='') as csv_file:
+            reader = csv.DictReader(csv_file)
 
-                for row in reader:
-                    p = DummyProject.objects.create(
-                        name=row['name'],
-                        project_description=row['project_description'],
-                        budget=row['budget'],
-                        zone=row['zone']
-                    )
-                    count += 1
+            for count, row in enumerate(reader):
+                p = DummyProject.objects.create(
+                    name=row['name'],
+                    project_description=row['project_description'],
+                    budget=row['budget'],
+                    zone=row['zone']
+                )
 
-                print(f'{count} objects saved to your local database. Happy development!')
+            print(f'{count} objects saved to your local database. Happy development!')
