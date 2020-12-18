68af46e74d6c480f6c3ec11757556853803eedde
Sam McAlilly <sammcalilly@gmail.com>
2020-12-18 15:32:43 -0600

    fixed data load command

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,3 +6,4 @@ set -euo pipefail
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
 python manage.py createcachetable && python manage.py clear_cache
+python manage.py load_development_data
