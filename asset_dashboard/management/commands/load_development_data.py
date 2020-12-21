19742c821733758ff485f3c41b7b75764bd328bc
Sam McAlilly <sammcalilly@gmail.com>
2020-12-21 09:36:09 -0600

    reverted change

--- asset_dashboard/management/commands/load_development_data.py
+++ asset_dashboard/management/commands/load_development_data.py
@@ -7,22 +7,19 @@ class Command(BaseCommand):
     help = 'Loads in dummy data for local development'
 
     def handle(self, *args, **options):
-        dummy_projects = DummyProject.objects.all()
+        with open('raw/simplified.csv', newline='') as csv_file:
+            reader = csv.DictReader(csv_file)
+            
+            count = 0
 
-        if len(dummy_projects) == 0:
-            with open('raw/simplified.csv', newline='') as csv_file:
-                reader = csv.DictReader(csv_file)
-                
-                count = 0
+            for row in reader:
+                p = DummyProject.objects.create(
+                    name=row['name'],
+                    project_description=row['project_description'],
+                    budget=row['budget'],
+                    zone=row['zone']
+                )
+                count += 1
 
-                for row in reader:
-                    p = DummyProject.objects.create(
-                        name=row['name'],
-                        project_description=row['project_description'],
-                        budget=row['budget'],
-                        zone=row['zone']
-                    )
-                    count += 1
-
-                print(f'{count} objects saved to your local database. Happy development!')
+            print(f'{count} objects saved to your local database. Happy development!')
             
