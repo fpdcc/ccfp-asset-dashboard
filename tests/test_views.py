c469d352eb8eab34065cdf1921d1344b0d1b5ba7
Sam McAlilly <sammcalilly@gmail.com>
2021-01-05 11:30:30 -0600

    improved tests for the project UpdateView

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,5 +1,5 @@
 import pytest
-from pytest_django.asserts import assertTemplateUsed
+from pytest_django.asserts import assertTemplateUsed, assertHTMLEqual
 from django.urls import reverse
 from asset_dashboard.models import Project
 
@@ -50,18 +50,18 @@ def test_add_project_view(client, section_owner):
 
 
 @pytest.mark.django_db
-def test_project_detail_view(client, project, section_owner):
+def test_project_update_view(client, project, project_list, section_owner):
     url = reverse('project-detail', kwargs={'pk': project.pk})
     response = client.get(url)
     assert response.status_code == 200
     assert response.context['project'] == project
 
-    # test bad url
+    # test bad url does not exist
     bad_url = reverse('project-detail', kwargs={'pk': 1234556873459})
     bad_response = client.get(bad_url)
     assert bad_response.status_code == 404
 
-    # test that the detail page can update the model instance
+    # test that the project UpdateView can update the model instance
     valid_form_data = {
         'name': 'trail improvement project',
         'description': 'We need to clean the trail, fix some washouts, and build a bathroom.',
@@ -71,3 +71,19 @@ def test_project_detail_view(client, project, section_owner):
     updated_project = Project.objects.filter(name=valid_form_data['name'])[0]
     assert updated_project.name == valid_form_data['name']
     assert updated_project.description == valid_form_data['description']
+
+    # test that the project updated and renders the updated data to the project ListView
+    response = client.get(reverse('projects'))
+    updated_project_name = Project.objects.filter(name=valid_form_data['name'])[0].name
+    name_submitted_with_form = valid_form_data['name']
+    assertHTMLEqual(
+        f'<td>{name_submitted_with_form}</td>',
+        f'<td>{updated_project_name}</td>'
+    )
+
+    # test that the project UpdateView did not overwrite any of the other projects
+    existing_project_name = Project.objects.filter(name='project_0')[0].name
+    assertHTMLEqual(
+        f'<td>{existing_project_name}</td>',
+        f'<td>project_0</td>'
+    )
