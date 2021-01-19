c0da6e5bf448452f0c1cae9120c4b6cc56bb9118
Sam McAlilly <sammcalilly@gmail.com>
2021-01-19 08:48:23 -0600

    missed an import

--- asset_dashboard/forms.py
+++ asset_dashboard/forms.py
@@ -1,5 +1,5 @@
 from django.forms import ModelForm, CharField, TextInput, Textarea
-from .models import Project, Section, ProjectScore
+from .models import Project, Section, ProjectScore, ProjectCategory
 
 class ProjectForm(ModelForm):
     class Meta:
