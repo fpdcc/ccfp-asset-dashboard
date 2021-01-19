55fd822bb73c72e8375fd9862e52efe7d086446e
Sam McAlilly <sammcalilly@gmail.com>
2021-01-19 08:40:01 -0600

    reverted to the category form

--- asset_dashboard/forms.py
+++ asset_dashboard/forms.py
@@ -35,3 +35,26 @@ class ProjectScoreForm(ModelForm):
             field.widget.attrs['min'] = min_validator.limit_value
             field.widget.attrs['max'] = max_validator.limit_value
             field.widget.attrs['class'] = 'form-control'
+
+class ProjectCategoryForm(ModelForm):
+    class Meta:
+        model = ProjectCategory
+        fields = [
+            'category',
+            'subcategory'
+        ]
+        # widgets = {
+        #     'category': Select(attrs={'class': 'form-control'}),
+        #     'subcategory': Select(attrs={'class': 'form-control'})
+        # }
+
+    def __init__(self, *args, **kwargs):
+        super(ProjectCategoryForm, self).__init__(*args, **kwargs)
+        # self.fields['category'].queryset = self.initial['category']
+        # self.fields['subcategory'].queryset = self.initial['subcategory']
+        category_choices = ProjectCategory.objects.values_list('id', 'category')
+        subcategory_choices = ProjectCategory.objects.values_list('id', 'subcategory')
+        category_widget = ChoiceField(widget=Select(choices=ProjectCategory.objects.values_list('id', 'category')), initial={'category': self.initial['category']})
+        subcategory_widget = ChoiceField(widget=Select(choices=ProjectCategory.objects.values_list('id', 'subcategory')), initial={'subcategory': self.initial['subcategory']})
+        self.fields['category'].widget = category_widget #Select(choices=ProjectCategory.objects.values_list('id', 'category')) #, initial={'category': self.initial['category']})
+        self.fields['subcategory'].widget = subcategory_widget #Select(choices=ProjectCategory.objects.values_list('id', 'subcategory')) #, initial={'subcategory': self.initial['subcategory']})
