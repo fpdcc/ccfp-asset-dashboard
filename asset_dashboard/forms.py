cd5720b1c0708bdc8a03442979d5beb57782deac
Sam McAlilly <sammcalilly@gmail.com>
2021-01-20 15:53:18 -0600

    cleaned up code

--- asset_dashboard/forms.py
+++ asset_dashboard/forms.py
@@ -7,10 +7,10 @@ class StyledFormMixin(object):
     """
     Generic mixin to consistently style each form field's html <input> element.
     """
-    
+
     def __init__(self, *args, **kwargs):
         super(StyledFormMixin, self).__init__(*args, **kwargs)
-        
+
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
 
