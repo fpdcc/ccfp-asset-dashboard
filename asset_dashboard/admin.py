75c6160964050014f7d0bc296219c8a80e542b11
Sam McAlilly <sammcalilly@gmail.com>
2021-02-02 14:58:41 -0600

    exclude name from admin

--- asset_dashboard/admin.py
+++ asset_dashboard/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from .models import CommissionerDistrict, HouseDistrict, Plan, Project, ProjectCategory, ProjectScore, Section, SenateDistrict, Staff, Zone
 
+class ProjectCategoryAdmin(admin.ModelAdmin):
+    exclude = ('name',)
+
+
 admin.site.register(CommissionerDistrict)
 admin.site.register(HouseDistrict)
 admin.site.register(Plan)
 admin.site.register(Project)
-admin.site.register(ProjectCategory)
+admin.site.register(ProjectCategory, ProjectCategoryAdmin)
 admin.site.register(ProjectScore)
 admin.site.register(Section)
 admin.site.register(SenateDistrict)
