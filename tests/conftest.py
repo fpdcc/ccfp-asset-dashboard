3ae80eef0cc549694f2bda02feba2d0ff77f0d06
Sam McAlilly <sammcalilly@gmail.com>
2021-03-09 15:24:54 -0600

    removed ScoreWeight fields from the test

--- tests/conftest.py
+++ tests/conftest.py
@@ -72,9 +72,7 @@ def districts():
 def score_weights():
     return models.ScoreWeights.objects.create(core_mission_score=0.7, operations_impact_score=0.8, 
                                               sustainability_score=0.2, ease_score=0.6, 
-                                              geographic_distance_score=0.5, social_equity_score=0.4,
-                                              obligation_weight=0.5, phase_completion=0.5, 
-                                              accessibility=0.5, leverage_resource=0.5)
+                                              geographic_distance_score=0.5, social_equity_score=0.4)
 
 @pytest.fixture
 def user():
