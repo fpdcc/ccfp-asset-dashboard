c1ff062020049b2522288da030e48e5971c99181
Sam McAlilly <sammcalilly@gmail.com>
2022-01-06 16:17:25 -0600

    save selected asset on the backend

--- asset_dashboard/static/js/components/maps/update.js
+++ asset_dashboard/static/js/components/maps/update.js
@@ -3,24 +3,31 @@ import React, { useState, useEffect } from 'react'
 import { GeoJSON } from 'react-leaflet'
 import BaseMap from '../maps/base'
 import AreaClipper from '../map-utils/clipper'
-import UpdateForm from '../maps/update-form'
 import Cookies from 'js-cookie'
+import Select from 'react-select'
 
 function UpdateMap(props) {
   const [searchedGeoms, setSearchedGeoms] = useState()
   const [existingGeoms, setExistingGeoms] = useState()
   const [clippedGeoms, setClippedGeoms] = useState(null)
+  const [phases, setPhases] = useState(null)
+  const [selectedPhase, setSelectedPhase] = useState(null)
 
   useEffect(() => {
     if (props?.search_geoms) {
       setSearchedGeoms(JSON.parse(props.search_geoms))
     }
 
-    // TODO: implement this on the backend
     if (props?.existing_geoms) {
       setExistingGeoms(JSON.parse(props.existing_geoms))
     }
-  }, [setSearchedGeoms, setExistingGeoms])
+
+    console.log(props)
+
+    const phaseOptions = makePhaseOptions(props.phases)
+    setPhases(phaseOptions)
+    setSelectedPhase(phaseOptions[0])
+  }, [setSearchedGeoms, setExistingGeoms, setPhases])
 
   function onMapCreated(map) {
     const group = new L.featureGroup()
@@ -37,47 +44,73 @@ function UpdateMap(props) {
   }
 
   function onClipped(featureCollection) {
-    console.log(JSON.stringify(featureCollection))
+     // TODO: implement the ability to click on a single asset? Or does one single way make it a better UX?
     setClippedGeoms(featureCollection)
   }
 
-  console.log(document.cookie)
-
   function saveGeometries() {
-    console.log('save them')
-    console.log(clippedGeoms)
-
     fetch(window.location.pathname, {
         headers: {
-            'Cookie': document.cookie,
             'Accept': 'application/json',
             'Content-Type': 'application/json',
             'X-CSRFTOKEN': Cookies.get('csrftoken')
         },
         method: 'POST',
-        body: JSON.stringify(clippedGeoms)
+        mode: 'same-origin',
+        body: JSON.stringify({
+          'geojson': clippedGeoms,
+          'phase': selectedPhase.value
+        })
     }).then((response) => {
-      if (response.status == 301) {
-        setClippedGeoms(null)
+      if (response.status == 200) {
+        location.reload()
+        // TODO: show success message
+        // https://stackoverflow.com/questions/13256817/django-how-to-show-messages-under-ajax-function
+      } else {
+        // TODO: catch error and show message
       }
     })
 }
 
+function makePhaseOptions(inputPhases) {
+  return inputPhases.map(phase => {
+    return {
+      value: phase.id.toString(),
+      label: `${phase.estimated_bid_quarter} - ${phase.phase_type} - ${phase.status}`
+    }
+  })
+}
+ 
   return (
     <div>
-      {clippedGeoms && 
-        <div>
-          <select>
-            {/* TODO: select so they can associate with a phase...is this neccessary? */}
-            {/* Do we need to associate an asset with a phase? How does this work when
-                an asset will inevitably get added to every phase? */}
-          </select>
-          <button onClick={() => saveGeometries()}>
+      {clippedGeoms 
+        ?
+        <div className='row'>
+          {/* This Select component is only for a proof of concept. We can tweak this however we need.
+           Not the best UI right now (that Z-index under the map!).*/}
+          {/* Do we need to associate an asset with a phase? How does this work when
+              an asset will inevitably get added to every phase? */}
+          <Select 
+            id='phases'
+            className='col-8'
+            value={selectedPhase}
+            options={phases}
+            onChange={(value) => setSelectedPhase(value)}
+            menuPosition='fixed' 
+            menuPortalTarget={document.body} />
+          <button 
+            className='btn btn-primary'
+            onClick={() => saveGeometries()}>
             Save Asset
           </button>
         </div>
+        : 
+        <div className='col-10'>
+          {/* // TODO: better languge!!! */}
+          <p>Select part of an asset, an entire asset, or a group of assets by holding shift and dragging a box over the assets.</p>
+        </div>
       }
-      <div className='map-viewer' aria-label='Asset Selection Map'>
+      <div className='map-viewer mt-5' aria-label='Asset Selection Map'>
         <BaseMap
           center={[41.8781, -87.6298]}
           zoom={11}
@@ -87,8 +120,8 @@ function UpdateMap(props) {
             geoJson={searchedGeoms}
             onClipped={onClipped} />
           {/* TODO: what colors do we want to use? do we need a legend? */}
-          <GeoJSON data={searchedGeoms} style={{color: 'green'}}/> 
-          <GeoJSON data={existingGeoms} style={{color: 'dark-green', opacity: 0.8}}/>
+          {searchedGeoms && <GeoJSON data={searchedGeoms} style={{color: 'green', opacity: 1}}/>}
+          {existingGeoms && <GeoJSON data={existingGeoms} style={{color: 'black'}}/>}
         </BaseMap>
       </div>
     </div>
