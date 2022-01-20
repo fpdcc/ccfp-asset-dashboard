2eb4f4ad9e157fc78f5d16e6a71be5adc41a7090
Sam McAlilly <sammcalilly@gmail.com>
2022-01-20 12:18:32 -0600

    extract table column config

---
+++ asset_dashboard/static/js/components/table_utils/assetSearchColumns.js
@@ -0,0 +1,12 @@
+const assetSearchColumns = [
+    {
+      Header: 'Identifier',
+      accessor: 'properties.identifier'
+    },
+    {
+      Header: 'Name',
+      accessor: 'properties.name', 
+    }
+  ]
+
+export default assetSearchColumns
