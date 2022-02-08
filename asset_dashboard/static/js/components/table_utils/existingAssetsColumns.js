eae2f89eeb43340245c010284c5e11d1fe5cffe1
Sam McAlilly <sammcalilly@gmail.com>
2022-02-08 10:34:39 -0600

    show success message in table

--- asset_dashboard/static/js/components/table_utils/existingAssetsColumns.js
+++ asset_dashboard/static/js/components/table_utils/existingAssetsColumns.js
@@ -1,6 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Cookies from 'js-cookie'
+import renderMessage from '../helpers/renderMessage'
 
-const existingAssetsColumns = (handleDelete) => {
+const existingAssetsColumns = () => {
+  const [ajaxMessage, setAjaxMessage] = useState(null)
+
+  function handleDelete(assetId) {
+    console.log('handld delete')
+    fetch(`/local-assets/${assetId}`, {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'X-CSRFTOKEN': Cookies.get('csrftoken')
+      },
+      method: 'DELETE',
+      mode: 'same-origin'
+    }).then((response) => {
+      console.log('response', response)
+      if (response.status == 204) {
+        setAjaxMessage('Asset successfully deleted.')
+        location.reload()
+      } else {
+        renderMessage('An error occurred when deleting the asset. Please try again.', 'danger')
+      }
+    }).catch(error => {
+      renderMessage('An error occurred when deleting the asset. Please try again.', 'danger')
+      console.error(error)
+    })
+  }
+
   return [
     {
       Header: 'Identifier',
@@ -18,7 +46,9 @@ const existingAssetsColumns = (handleDelete) => {
       Header: () => null,
       accessor: 'id',
       disableSortBy: true,
-      Cell: props => (<button 
+    Cell: props => (ajaxMessage 
+                      ? <p>{ajaxMessage}</p>
+                      : <button 
                         className='btn btn-outline-danger' 
                         onClick={() => handleDelete(props.value)}>
                           <i className='fas fa-trash'></i> Delete
--- asset_dashboard/static/js/components/tables/ExistingAssetsTable.js
+++ asset_dashboard/static/js/components/tables/ExistingAssetsTable.js
@@ -1,37 +1,35 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Cookies from 'js-cookie'
 import ReactTable from '../BaseTable'
 import existingAssetsColumns from '../table_utils/existingAssetsColumns'
-import renderMessage from '../helpers/renderMessage'
 
 function ExistingAssetsTable({ rows }) {
-  function handleDelete(assetId) {
-    fetch(`/local-assets/${assetId}`, {
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'X-CSRFTOKEN': Cookies.get('csrftoken')
-      },
-      method: 'DELETE',
-      mode: 'same-origin'
-    }).then((response) => {
-      console.log('response', response)
-      if (response.status == 204) {
-        renderMessage('Assets successfully deleted.', 'success')
-        location.reload()
-      }
-    }).catch(error => {
-      renderMessage('An error occurred when deleting the asset. Please try again.', 'danger')
-      console.error(error)
-    })
-  }
+  // function handleDelete(assetId) {
+  //   fetch(`/local-assets/${assetId}`, {
+  //     headers: {
+  //       'Accept': 'application/json',
+  //       'Content-Type': 'application/json',
+  //       'X-CSRFTOKEN': Cookies.get('csrftoken')
+  //     },
+  //     method: 'DELETE',
+  //     mode: 'same-origin'
+  //   }).then((response) => {
+  //     console.log('response', response)
+  //     if (response.status == 204) {
+  //       renderMessage('Assets successfully deleted.', 'success')
+  //       location.reload()
+  //     }
+  //   }).catch(error => {
+  //     renderMessage('An error occurred when deleting the asset. Please try again.', 'danger')
+  //     console.error(error)
+  //   })
+  // }
 
   return (
     <>
       <ReactTable
         rows={rows}
-        columns={React.useMemo(() =>  existingAssetsColumns(handleDelete), [])}
+        columns={React.useMemo(() =>  existingAssetsColumns(), [])}
         pageSizeIncrements={[10, 20, 30]}
         sizeOfPage={10}
       />
