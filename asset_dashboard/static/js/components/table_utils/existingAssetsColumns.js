b722432de0ae7c2824dbfcda931a12d3fceba941
Sam McAlilly <sammcalilly@gmail.com>
2022-02-08 10:46:07 -0600

    better message handling in existing assets table

--- asset_dashboard/static/js/components/table_utils/existingAssetsColumns.js
+++ asset_dashboard/static/js/components/table_utils/existingAssetsColumns.js
@@ -1,34 +1,6 @@
-import React, { useState } from 'react'
-import Cookies from 'js-cookie'
-import renderMessage from '../helpers/renderMessage'
+import React from 'react'
 
-const existingAssetsColumns = () => {
-  const [ajaxMessage, setAjaxMessage] = useState(null)
-
-  function handleDelete(assetId) {
-    console.log('handld delete')
-    fetch(`/local-assets/${assetId}`, {
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'X-CSRFTOKEN': Cookies.get('csrftoken')
-      },
-      method: 'DELETE',
-      mode: 'same-origin'
-    }).then((response) => {
-      console.log('response', response)
-      if (response.status == 204) {
-        setAjaxMessage('Asset successfully deleted.')
-        location.reload()
-      } else {
-        renderMessage('An error occurred when deleting the asset. Please try again.', 'danger')
-      }
-    }).catch(error => {
-      renderMessage('An error occurred when deleting the asset. Please try again.', 'danger')
-      console.error(error)
-    })
-  }
-
+const existingAssetsColumns = (handleDelete) => {
   return [
     {
       Header: 'Identifier',
@@ -46,9 +18,7 @@ const existingAssetsColumns = () => {
       Header: () => null,
       accessor: 'id',
       disableSortBy: true,
-    Cell: props => (ajaxMessage 
-                      ? <p>{ajaxMessage}</p>
-                      : <button 
+      Cell: props => (<button 
                         className='btn btn-outline-danger' 
                         onClick={() => handleDelete(props.value)}>
                           <i className='fas fa-trash'></i> Delete
--- asset_dashboard/static/js/components/tables/ExistingAssetsTable.js
+++ asset_dashboard/static/js/components/tables/ExistingAssetsTable.js
@@ -1,39 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
+import Cookies from 'js-cookie'
 import ReactTable from '../BaseTable'
 import existingAssetsColumns from '../table_utils/existingAssetsColumns'
 
 function ExistingAssetsTable({ rows }) {
-  // function handleDelete(assetId) {
-  //   fetch(`/local-assets/${assetId}`, {
-  //     headers: {
-  //       'Accept': 'application/json',
-  //       'Content-Type': 'application/json',
-  //       'X-CSRFTOKEN': Cookies.get('csrftoken')
-  //     },
-  //     method: 'DELETE',
-  //     mode: 'same-origin'
-  //   }).then((response) => {
-  //     console.log('response', response)
-  //     if (response.status == 204) {
-  //       renderMessage('Assets successfully deleted.', 'success')
-  //       location.reload()
-  //     }
-  //   }).catch(error => {
-  //     renderMessage('An error occurred when deleting the asset. Please try again.', 'danger')
-  //     console.error(error)
-  //   })
-  // }
+  const [ajaxMessage, setAjaxMessage] = useState(null)
+
+  function handleDelete(assetId) {
+    fetch(`/local-assets/${assetId}`, {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'X-CSRFTOKEN': Cookies.get('csrftoken')
+      },
+      method: 'DELETE',
+      mode: 'same-origin'
+    }).then((response) => {
+      console.log('response', response)
+      if (response.status == 204) {
+        setAjaxMessage('Asset successfully deleted.')
+        location.reload()
+      }
+    }).catch(error => {
+      setAjaxMessage('An error occurred when deleting the asset. Please try again.')
+      console.error(error)
+    })
+  }
 
   return (
-    <>
+    <div>
+      <h3>Phase Assets</h3>
+      {ajaxMessage ? <p className='text-center alert alert-success'>{ajaxMessage}</p> : null}
       <ReactTable
         rows={rows}
-        columns={React.useMemo(() =>  existingAssetsColumns(), [])}
+        columns={React.useMemo(() =>  existingAssetsColumns(handleDelete), [])}
         pageSizeIncrements={[10, 20, 30]}
         sizeOfPage={10}
       />
-    </>
+    </div>
   )
 }
 
