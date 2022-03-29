95bb4479bfe39f0ec1195aa712c8ca6e73760c77
Sam McAlilly <sammcalilly@gmail.com>
2022-03-29 16:25:40 -0500

    fix point popup bug

--- asset_dashboard/static/js/components/map_utils/ShowPopup.js
+++ asset_dashboard/static/js/components/map_utils/ShowPopup.js
@@ -7,15 +7,20 @@ export default function ShowPopup({ geojson }) {
   useEffect(() => {
     map.eachLayer((layer) => {
       if (layer.feature == geojson) {
+        console.log('layer.feature == geojson')
+        console.log('layer.feature', layer.feature)
+        console.log('geojson', geojson)
         layer.openPopup()
 
         // Make the layer appear selected.
-        layer.setStyle({
-          fillColor: '#3388ff',
-          fillOpacity: '0.2',
-          color: '#3388ff',
-          weight: '4'
-        })
+        if (layer.setStyle) {
+          layer.setStyle({
+            fillColor: '#3388ff',
+            fillOpacity: '0.2',
+            color: '#3388ff',
+            weight: '4'
+          })
+        }
       }
     })
   }, [geojson])
