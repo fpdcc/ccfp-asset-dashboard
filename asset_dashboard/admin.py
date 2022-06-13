3bb61a64537d47448725ab9b118dcd77e1102868
Sam McAlilly <sammcalilly@gmail.com>
2022-06-13 15:07:44 -0500

    remove more pieces of functionality from admin interface

--- asset_dashboard/admin.py
+++ asset_dashboard/admin.py
@@ -2,13 +2,8 @@ from django.contrib import admin
 from . import models
 
 
-admin.site.register(models.CommissionerDistrict)
-admin.site.register(models.HouseDistrict)
 admin.site.register(models.Project)
 admin.site.register(models.ProjectCategory)
-admin.site.register(models.ProjectScore)
 admin.site.register(models.ScoreWeights)
 admin.site.register(models.Section)
-admin.site.register(models.SenateDistrict)
 admin.site.register(models.Staff)
-admin.site.register(models.Zone)
