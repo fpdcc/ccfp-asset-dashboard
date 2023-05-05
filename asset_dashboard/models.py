f08644426a4003445d734e2d713d64f7599b811f
Sam McAlilly <smcalilly@gmail.com>
2023-05-05 12:04:00 -0500

    appease flake8

--- asset_dashboard/models.py
+++ asset_dashboard/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.contrib.gis.db import models
 from django.db.models import Max, Sum, QuerySet
-from django.db.models.signals import post_save, post_delete, pre_delete
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from django.contrib.gis.geos import GEOSGeometry, GeometryCollection
 from django.conf import settings
