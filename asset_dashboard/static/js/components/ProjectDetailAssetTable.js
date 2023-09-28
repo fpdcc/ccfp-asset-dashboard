66c0cc2fa401b0768c0ef8cdd6a11bd7f1b255b8
Sam McAlilly <38969506+smcalilly@users.noreply.github.com>
2023-09-28 09:14:30 -0500

    Fix javascript loading bug (#278)
    * try some javascript magic
    
    * move js block out of inherited block + rearrange functions based on runtime
    
    * user original code, just put it in the correct place

--- asset_dashboard/static/js/components/ProjectDetailAssetTable.js
+++ asset_dashboard/static/js/components/ProjectDetailAssetTable.js
@@ -7,7 +7,7 @@ function ProjectDetailAssetTable(props) {
     <div>
       <div className="d-flex align-items-center justify-content-between m-2 col-4">
         <h3>Project Assets</h3>
-        <a href={`/projects/phases/edit/${props.assets.features[0].properties.phase}/assets`} class="text-info lead">Edit Assets ></a>
+        <a href={`/projects/phases/edit/${props.assets.features[0].properties.phase}/assets`} className="text-info lead">Edit Assets ></a>
       </div>
       <AssetsListMap assets={props.assets} />
     </div>
