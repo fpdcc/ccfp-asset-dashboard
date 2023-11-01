a2a99dea8e201cd7dc0652d8da9feff8b90b120e
Forest Gregg <fgregg@datamade.us>
2023-11-01 10:36:32 -0400

    get handle on deployment id in pong another way

--- asset_dashboard/views.py
+++ asset_dashboard/views.py
@@ -446,7 +446,7 @@ def pong(request):
     from django.http import HttpResponse
 
     try:
-        from ..deployment import DEPLOYMENT_ID
+        from settings import DEPLOYMENT_ID
     except ImportError as e:
         return HttpResponse('Bad deployment: {}'.format(e), status=401)
 
