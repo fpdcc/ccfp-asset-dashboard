d38f1ba247aeabfdd37dd9c68df9dbdb3522f6f8
Xavier Medrano <xaviermedrano@macbookpro.lan>
2025-07-11 14:59:11 -0400

    cut down on number of assets to save

--- asset_dashboard/management/commands/recalculate_project_geos.py
+++ asset_dashboard/management/commands/recalculate_project_geos.py
@@ -9,5 +9,9 @@ class Command(BaseCommand):
     help = "Recalculate and save geographies for all projects."
 
     def handle(self, *args, **options):
+        # Saving an asset will trigger an update for that asset's phase and project
+        projects_updated = []
         for l in tqdm(LocalAsset.objects.all()):
-            l.save()
+            if l.phase.project.id not in projects_updated:
+                l.save()
+                projects_updated.append(l.phase.project.id)
